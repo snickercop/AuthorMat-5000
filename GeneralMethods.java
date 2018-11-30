6745d80d949fd3c4e40897eefcd853c07bd05588
unknown <allegracopland@brandeis.edu>
2018-11-30 03:09:47 -0500

    now works outside of eclipse

---
+++ GeneralMethods.java
@@ -0,0 +1,10 @@
+
+public class GeneralMethods {
+	public static double[] addArrays(double[] array1, double[] array2) {
+		double[] output = new double[array1.length];
+		for(int i = 0; i < array1.length; i++) {
+			output[i] = array1[i] + array2[i];
+		}
+		return output;
+	}
+}
